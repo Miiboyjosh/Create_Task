4fdc4065b2ae8d54be59b7612885b57be55cebfe
Joshua <joshua.jean@vtsd.com>
2024-02-08 07:58:22 -0500

    Day_1

---
+++ pokemon_starter_quiz.py
@@ -0,0 +1,19 @@
+regionList = ["Alolan", "Galar", "Hoenn", "Johto", "Kalos", "Kanto", "Paldea", "Sinnoh", "Unova"]
+alolaList = ["Rowlet", "Litten", "Popplio"]
+galarList = ["Grookey", "Scorbunny", "Sobble"]
+hoennList = ["Treecko", "Torchic", "Mudkip"]
+johtoList = ["Chikorita", "Cyndaquil", "Totodile"]
+kalosList = ["Chespin", "Fennekin", "Froakie"]
+kantoList = ["Bulbasaur", "Charmander", "Squirtle"]
+paldeaList = ["Sprigatito", "Fuecoco", "Quaxly"]
+sinnohList = ["Turtwig", "Chimchar", "Piplup"]
+unovaList = ["Snivy", "Tepig", "Oshawtt"]
+
+#Greet the User
+trainer_name = input("What is your name?")
+print("Welcome",trainer_name, "to your Perfect Pokemon Match!")
+#Give the user a brief description
+print("This quiz will help you find you best suiting starter pokemon to join you on your pokemon adventure")
+#Ask the First Question
+def ques_1():
+
