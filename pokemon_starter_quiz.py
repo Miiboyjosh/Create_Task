359d044fc0ade268fe13ba696152cb936b20e2ae
Joshua <joshua.jean@vtsd.com>
2024-02-14 07:58:12 -0500

    Day_4

--- pokemon_starter_quiz.py
+++ pokemon_starter_quiz.py
@@ -11,16 +11,28 @@ unovaList = ["Snivy", "Tepig", "Oshawtt"]
 
 #Greet the User
 trainer_name = input("What is your name?")
-print("Welcome",trainer_name, "to your Perfect Pokemon Match!")
-#Give the user a brief description
+print("Welcome", trainer_name, "to your Perfect Pokemon Match!")
+# Give the user a brief description
 print("This quiz will help you find you best suiting starter pokemon to join you on your pokemon adventure")
-#Ask the First Question
+# Ask the First Question
 def ques_1():
-    global answer
+    global regions, answer
     print("Where is the Place You Would Associate Yourself With?")
     print("1. Hawaii\n2. The United Kingdom\n3. Japan\n4. Metropolitan France\n5. Spain\n6. New York City")
-    input("\nAnswer:")
-    for regions in regionList:
-        if answer == 3:
-            print()
+    response = input("\nAnswer:")
+    if response == "3":
+        print("Your Region is Japan")
+
+
+def score_sys(answer):
+    global score
+    if answer == "A":
+        score += 1
+    if answer == "B":
+        score += 2
+    if answer == "C":
+        score += 3
+
+
+
 ques_1()
