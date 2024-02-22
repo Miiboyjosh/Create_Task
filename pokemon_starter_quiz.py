0e98f572019d516fc240706ce1c3d3b543d8715f
Joshua <joshua.jean@vtsd.com>
2024-02-22 10:08:24 -0500

    Changes by 20288

--- pokemon_starter_quiz.py
+++ pokemon_starter_quiz.py
@@ -1,15 +1,16 @@
-regionList = ["Alolan", "Galar", "Hoenn", "Johto", "Kalos", "Kanto", "Paldea", "Sinnoh", "Unova"]
-alolaList = ["Rowlet", "Litten", "Popplio"]
-galarList = ["Grookey", "Scorbunny", "Sobble"]
-hoennList = ["Treecko", "Torchic", "Mudkip"]
-johtoList = ["Chikorita", "Cyndaquil", "Totodile"]
-kalosList = ["Chespin", "Fennekin", "Froakie"]
-kantoList = ["Bulbasaur", "Charmander", "Squirtle"]
-paldeaList = ["Sprigatito", "Fuecoco", "Quaxly"]
-sinnohList = ["Turtwig", "Chimchar", "Piplup"]
-unovaList = ["Snivy", "Tepig", "Oshawtt"]
-
-#Greet the User
+# Self identifying lists
+alolaList = ["Litten", "Rowlet", "Popplio"]
+galarList = ["Scorbunny", "Grookey", "Sobble"]
+hoennList = ["Torchic", "Treeko", "Mudkip"]
+johtoList = ["Cyndaquil", "Chikorita", "Totodile"]
+kalosList = ["Fennekin", "Chespin", "Froakie"]
+kantoList = ["Charmander", "Bulbasaur", "Squirtle"]
+paldeaList = ["Fuecoco", "Sprigatito", "Quaxly"]
+sinnohList = ["Chimchar", "Turtwig", "Piplup"]
+unovaList = ["Tepig", "Snivy", "Oshawtt"]
+# Variables
+score = 0
+# Greet the User
 trainer_name = input("What is your name?")
 print("Welcome",trainer_name,"to your Perfect Pokemon Match!")
 # Give the user a brief description
@@ -22,28 +23,47 @@ def ques_1():
     response = input("\nAnswer:")
     if response == "1":
         print("Your region is Alola")
+        region = alolaList
         ques_2()
     if response == "2":
         print("Your region is Galar")
+        region = galarList
         ques_2()
     if response == "3":
         japan_opt()
     if response == "4":
         print("Your region is Kalos")
+        region = kalosList
         ques_2()
     if response == "5":
         print("Your region is Paldea")
+        region = paldeaList
         ques_2()
     if response == "6":
         print("Your region is Unova")
+        region = unovaList
         ques_2()
 def japan_opt():
+    global region
     print("Which of the Follow Geographies Would You Prefer")
     print("1. Volcanic\n2. Plains\n3. Mountainous\n4. Archipelago")
-    response = input("\nAnswer:")
-    if response == "1":
-
-
+    jap_response = input("\nAnswer:")
+    if jap_response == "1":
+        print("Your region is Hoenn")
+        region = hoennList
+        ques_2()
+    if jap_response == "2":
+        print("Your region is Kanto")
+        region = kantoList
+        ques_2()
+    if jap_response == "3":
+        print("Your region is Sinnoh")
+        region = sinnohList
+        ques_2()
+    if jap_response == "4":
+        print("Your region is Johto")
+        region = johtoList
+        ques_2()
 def ques_2():
     print("What characteristic best describes you")
     print("A. Passionate\nB. Relaxed\nC. Sympathetic")
@@ -64,7 +84,7 @@ def ques_4():
     ques_5()
 def ques_5():
     print("Which of these hobbies do you enjoy most?")
-    print("A. Hiking\nB. Reading\nC. Weightlifting")
+    print("Weightlifting\nB. Reading\nC. Hiking")
     answer = input("\nAnswer:")
     score_sys(answer)
 def score_sys(answer):
@@ -76,7 +96,13 @@ def score_sys(answer):
     if answer == "C" or "c":
         score += 1
 def fin_results():
-    if score >= 5:
-        print()
+    if score <= 12:
+        print(region[0])
+    if score <= 8:
+        print(region[1])
+    if score <= 4:
+        print(region[2])
+
 
 ques_1()
+fin_results()
